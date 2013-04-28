d901b203643cdd0188653c5b4ef5dd7b1ab50f65
David Rankin <Rankida@gmail.com>
2013-04-28 16:43:22 +0100

    Added the env to the console.log at the start of the node server.

--- server.js
+++ server.js
@@ -78,5 +78,5 @@ require('./apps/mobile/routes')(app, WeightHistory, mongoose, loginRequired);
 require('./apps/colourflash/colourFlash')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
